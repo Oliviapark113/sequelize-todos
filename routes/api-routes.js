bfaff15413eb82c97034ddbbb8649826f0735f1b
Olivia Park <oliviaypark113@gmail.com>
2021-02-16 10:10:23 -0500

    finial updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,56 +1,65 @@
 const express = require('express')
-
 const router = express.Router()
-
 const db = require('../models')
+
 
 router.get('/api/todos', async(req, res)=>{
     try{
-
-     const todos =  await db.Todo.findAll()
-     res.json(todos)
-
-    } catch(err){
-        res.status(500).send(err)
-    }
-
-})
-
-router.post('/api/todos', async (req, res)=>{ 
-
-    try{
-        await db.Todo.create(req.body)
-        res.status(200).send()
-    } catch(err){
-         res.status(500).send(err)
-    }
-
-})
-
-router.put('/api/todos/:id',async(req, res) =>{
-    try{
-    const id = req.params.id
-    await db.Todo.update(req.body, {
-        where:{ id }
-    })
-
+        //db.Todo.findAll() will return array of objects .. save into variables 
+      const todos = await db.Todo.findAll()
+      //send whole list of json data to front end 
+      res.json(todos)
 
     }catch(err){
-        res.status(500).send(err) 
+    res.status(500).send(err)
     }
+
 
 })
 
-router.delete('/api/todos/:id',async (req, res)=>{
+router.post('/api/todos', async (req, res)=>{
+  //async await function ... 
+  //wait for create.. 
+  try{
+
+    await db.Todo.create(req.body)
+    res.status(200).send()
+  }
+  catch(err){
+     res.status(500).send(err)
+  }  
+
+})
+
+router.put('/api/todos/:id', async (req, res) => {
+
+  try {
+    const id = req.params.id
+
+    console.log(req.body)
+
+    await db.Todo.update(req.body, {
+      where: { id }
+    })
+    res.status(200).send()
+  } catch(err) {
+    res.status(500).send(err)
+  }
+})
+
+
+
+router.delete('/api/todos/:id', async(req, res)=>{
     try{
-      const id = req.params.id
-      await db.Todo.destroy({
-          where:{ id }
+        const id = req.params.id
+        await db.Todo.destroy({
+           where:{id}
         })
         res.status(200).send()
     }catch(err){
-    res.status(500).send(err) }
-    
+        res.status(500).send()
+    }
+
 })
 
-module.exports = router 
+module.exports = router
